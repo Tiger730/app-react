5f0be80ffdc959091fc34ef602fe0159341742c5
Tiger730 <jakkrit.mo.61@ubu.ac.th>
2021-02-24 00:10:26 +0700

    Updates

---
+++ precache-manifest.45794a9d2f0d9d112f73ab769ac7d801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb8335e126326e0ebc06aa9a0cc1746",
+    "url": "/app-react/index.html"
+  },
+  {
+    "revision": "5e50a6c87e5e345371d6",
+    "url": "/app-react/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9477aa090d40052fc83a",
+    "url": "/app-react/static/css/main.70d8e105.chunk.css"
+  },
+  {
+    "revision": "5e50a6c87e5e345371d6",
+    "url": "/app-react/static/js/2.b6a7e599.chunk.js"
+  },
+  {
+    "revision": "9477aa090d40052fc83a",
+    "url": "/app-react/static/js/main.26c1ce9f.chunk.js"
+  },
+  {
+    "revision": "a83a0080f83586ae7171",
+    "url": "/app-react/static/js/runtime-main.9d832454.js"
+  }
+]);
